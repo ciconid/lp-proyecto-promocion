9a7f8d7e8373098f7d846c9a6d1ff669ef4cf1b6
tombertotto <tom.bertotto@gmail.com>
2026-06-29 18:35:25 -0300

    Implementar busqueda de todos los docs en archivos y agregar archivos nuevos

--- src/main/scala/resumen/io/LectorDocumentos.scala
+++ src/main/scala/resumen/io/LectorDocumentos.scala
@@ -10,12 +10,23 @@ object LectorDocumentos {
     val dir = new File(ruta)
     if (!dir.isDirectory) List.empty
     else
-      Option(dir.listFiles((_, nombre) => nombre.toLowerCase.endsWith(".txt")))
-        .map(_.toList)
-        .getOrElse(List.empty)
-        .sortBy(_.getName)
-        .map(archivo => archivo.getName -> leerArchivo(archivo))
+      buscarArchivos(dir)
+        .sortBy(archivo => rutaRelativa(dir, archivo))
+        .map(archivo => rutaRelativa(dir, archivo) -> leerArchivo(archivo))
   }
+
+  private def buscarArchivos(dir: File): List[File] =
+    Option(dir.listFiles())
+      .map(_.toList)
+      .getOrElse(List.empty)
+      .flatMap { archivo =>
+        if (archivo.isDirectory) buscarArchivos(archivo)
+        else if (archivo.getName.toLowerCase.endsWith(".txt")) List(archivo)
+        else List.empty
+      }
+
+  private def rutaRelativa(base: File, archivo: File): String =
+    base.toPath.relativize(archivo.toPath).toString.replace('\\', '/')
 
   private def leerArchivo(archivo: File): String =
     Using.resource(Source.fromFile(archivo, "UTF-8"))(_.mkString)
